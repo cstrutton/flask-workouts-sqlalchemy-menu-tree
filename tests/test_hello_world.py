42ad4e861a47ebc5e77236bddf25df7d23e4150d
Chris Strutton <chris@rodandfly.ca>
2015-07-12 16:26:03 +0000

    get basic testing working

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -3,20 +3,24 @@ from flask import current_app
 from app import create_app #, db
 
 
-class BasicsTestCase(unittest.TestCase):
+class HelloWorldTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('testing')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+        self.test_client = self.app.test_client()
 #       db.create_all()
 
     def tearDown(self):
 #       db.session.remove()
 #       db.drop_all()
-        self.app_context.pop()
+        pass
 
     def test_app_exists(self):
-        self.assertFalse(current_app is None)
+        self.assertFalse(self.app is None)
     
     def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
+        self.assertTrue(self.app.config['TESTING'])
+        
+    def test_index_returns_helloworld(self):
+        rv = self.test_client.get('/')
+        assert 'hello world' in rv.data        
+        
